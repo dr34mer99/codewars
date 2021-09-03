5f9a83b1b35807f8f23c0991f28e3bfd89d6e78f
Patryk <dr34mer00@gmail.com>
2021-09-03 21:42:28 +0200

    updt

---
+++ kyu5/k primes.py
@@ -0,0 +1,13 @@
+def printKPFNums(A, B, K):
+    prime = []
+    p_factors = [0] * (B + 1)
+    for p in range(2, B + 1):
+        if p_factors[p] == 0:
+            for i in range(p, B + 1, p):
+                p_factors[i] = p_factors[i] + 1
+    for i in range(A, B + 1):
+        if p_factors[i] == K:
+            prime.append(i)
+    return prime
+
+print(printKPFNums(10,23,2))
